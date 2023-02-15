b3c619d39c500726893301b59b315ae07850242f
Basem Osama <engbasemosama@gmail.com>
2023-02-15 21:02:40 +0200

    CU-3ufnprh - app version widget

--- lib/app/splash/ui/controllers/splash_controller.dart
+++ lib/app/splash/ui/controllers/splash_controller.dart
@@ -1,18 +1,11 @@
 import 'package:flutter_boilerplate/core/preferences/preference_manger.dart';
-import 'package:package_info_plus/package_info_plus.dart';
 import 'package:playx/playx.dart';
 
 import '../../../../core/navigation/app_navigation.dart';
 
 class SplashController extends GetxController {
-  RxString versionName = 'V'.obs;
-
   @override
-  Future<void> onInit() async {
-    final PackageInfo packageInfo = await PackageInfo.fromPlatform();
-    final String version = packageInfo.version;
-    versionName.value = "V$version";
-
+  void onInit() {
     final bool isUserLoggedIn = MyPreferenceManger().isLoggedIn;
 
     twoSeconds().then((_) {
