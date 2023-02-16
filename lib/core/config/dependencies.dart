8ebbe593f8f1cc1cb54416d79fb97647cf121a35
Basem Osama <engbasemosama@gmail.com>
2023-02-16 19:01:07 +0200

    CU-3kavzaa_feature-integrate-Sentry_Basem-Osama

--- lib/core/config/dependencies.dart
+++ lib/core/config/dependencies.dart
@@ -2,11 +2,13 @@ import 'package:dio/dio.dart';
 import 'package:fimber/fimber.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:playx/playx.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 import '../navigation/app_navigation.dart';
 import '../network/api_client.dart';
 import '../network/dio_client.dart';
 import '../preferences/preference_manger.dart';
+import 'keys.dart';
 
 Future<void> boot() async {
   // setup and boot your dependencies here
@@ -15,6 +17,9 @@ Future<void> boot() async {
 
   //USED FOR DEBUGGING
   Fimber.plantTree(DebugTree());
+  await SentryFlutter.init((options) {
+    options.dsn = Keys.sentryKey;
+  });
 
   final preferenceManger = MyPreferenceManger();
   Get.put<MyPreferenceManger>(preferenceManger);
